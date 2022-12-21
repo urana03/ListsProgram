970b68e07fe2323b7a074e034c56fbbc8eefd34f
urana03 <uhackninja@gmail.com>
2022-12-21 08:07:02 +0530

    Add: To insert the element in the linked list

--- src/main/java/com/bridgelabz/linkedlist/SimpleList.java
+++ src/main/java/com/bridgelabz/linkedlist/SimpleList.java
@@ -25,16 +25,40 @@ public class SimpleList {
 	
 	}
 	
-	public boolean search(Node head,int x) {
+  public boolean search(Node head,int x) {
 		
 		      Node current = head;
 	           while(current != null) {
 			    if(current.data == x) 
 				   return true;
-				   current = current.next;
+				   current = current.next ;
 			   }
 			   return false; 
 		}
+	
+	public void insertAfter(Node prevNode, int data)
+	{
+	
+		if (prevNode == null) {
+	      
+			System.out.println("The given previous node cannot be null");
+	        return;
+	      }
+		 
+		Node newNode = new Node(data);
+
+	    prevNode.next = newNode;
+	}
+	
+
+	public void printList() {
+	
+		Node n = head;
+		while(n != null) {
+			System.out.print(n.data+" ");
+			n=n.next;
+		}
+	}
 
 	public static void main(String args[]) {
 		SimpleList list = new SimpleList();
@@ -43,12 +67,13 @@ public class SimpleList {
 		list.append(30);
 		list.append(70);
 		
-		if(list.search(list.head,30)) {
-			System.out.print("Yes");
+		if(list.search(list.head,30)==true) {
+			list.insertAfter(list.head,40);
 		}
 		else {
-			System.out.print("No");
+			System.out.print("No where to insert");
 		}
 		
+		list.printList();
 	}
 }
